da66f8dc9ddd8f0847bcd74ee7721550a115392e
Yusuke Nojima <nojima@ynojima.com>
2019-11-13 11:14:41 +0900

    組み込みの Phong シェーダを使ってみる

--- main.js
+++ main.js
@@ -8,12 +8,18 @@ function main() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+    var ambientLight = new THREE.AmbientLight(0x404040);
+    scene.add(ambientLight);
+
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    scene.add(directionalLight);
+
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var material = new THREE.MeshPhongMaterial({ color: 0xaaaaff });
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    camera.position.z = 5;
+    camera.position.z = 2;
 
     function animate() {
         requestAnimationFrame(animate);
